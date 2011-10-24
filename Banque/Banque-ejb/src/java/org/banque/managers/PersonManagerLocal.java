1dbfc8d4f71b0761508d7d6b546b1ee6aad714b8
Bruno Jurkovski <bruno91j@gmail.com>
2011-10-24 17:52:46 -0200

    Adding PersonManager.

---
+++ Banque/Banque-ejb/src/java/org/banque/managers/PersonManagerLocal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.banque.managers;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author bjurkovski
+ */
+@Local
+public interface PersonManagerLocal {
+    
+}
